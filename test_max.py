276af869df11d330e063af7a79e7d574748887c7
ariana-ya <pintusik22@mail.ru>
2022-10-16 00:28:15 +0300

    djd

---
+++ test_max.py
@@ -0,0 +1,18 @@
+import unittest
+from functions import _max
+
+class Min(unittest.TestCase):
+    def test_max(self):
+        self.assertEqual(_max([404, 505, 808]), 808)
+
+    def test_max1(self):
+        self.assertEqual(_max([-123456789, -12345, 0]), 0)
+
+    def test_max2(self):
+        self.assertEqual(_max([333/111, 333/3, 333/9]), 111)
+
+    def test_max3(self):
+        self.assertEqual(_max([9*9, 9**9, 9+9*9]), 387420489)
+
+    def test_max4(self):
+        self.assertEqual(_max([7, 7, 7, 7, 7]), 7)
