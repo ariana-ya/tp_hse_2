4c0a343f97f6ca66c124986a61fe4f4abef0267d
ariana-ya <pintusik22@mail.ru>
2022-10-16 11:31:59 +0300

    ff

--- test_like.py
+++ test_like.py
@@ -1,6 +1,7 @@
 import unittest
 from functions import _sum, _mult
 
+
 class MinMax(unittest.TestCase):
     def test_like(self):
         self.assertGreater(_mult([1, 2, 3, 4, 5]), _sum([1, 2, 3, 4, 5]))
